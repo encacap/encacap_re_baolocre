8dac51aab8cc90f5039d83ef4c4f84eac6858534
Nguyen Khac Khanh <encacap@gmail.com>
2022-04-29 10:41:29 +0700

    style: change search bg-color

--- src/features/HomePage/components/SearchSection.tsx
+++ src/features/HomePage/components/SearchSection.tsx
@@ -1,6 +1,6 @@
 const SearchSection = () => (
     <section className="relative z-10 overflow-hidden bg-white border-b-2 border-gray-100 md:-top-14 md:border-b-0 md:shadow-lg md:shadow-gray-100 md:rounded-md md:mx-4 lg:mx-10 xl:mx-48">
-        <div className="items-center hidden px-4 py-4 font-medium text-white uppercase md:flex bg-encacap-main md:px-6">
+        <div className="items-center hidden px-4 py-4 font-medium text-white uppercase md:flex bg-encacap-blue md:px-6">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" className="w-4 mr-2">
                 <path
                     d="M11.5 21a9.5 9.5 0 1 0 0-19 9.5 9.5 0 0 0 0 19ZM22 22l-2-2"
