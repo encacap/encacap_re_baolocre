226afd698c75fe2559a3a46b3d59e1c3fa69d6db
Nguyen Khac Khanh <encacap@gmail.com>
2022-06-07 22:12:37 +0700

    feat(ui): complete REDImageSliderImage

--- src/features/Estates/EstateDetail/Components/REDImageSlider.tsx
+++ src/features/Estates/EstateDetail/Components/REDImageSlider.tsx
@@ -4,6 +4,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { NavigationOptions, PaginationOptions } from "swiper/types";
+import REDImageSliderImage from "./REDImageSliderImage";
 import REDImageSliderNextButton from "./REDImageSliderNextButton";
 import REDImageSliderPrevButton from "./REDImageSliderPrevButton";
 
@@ -11,7 +12,7 @@ const REDImageSlider = () => {
     const pagination: PaginationOptions = {
         type: "fraction",
         horizontalClass:
-            "absolute z-10 bottom-4 lg:bottom-10 right-4 lg:right-10 bg-black bg-opacity-50 px-4 py-1 rounded-md text-white text-sm font-semibold",
+            "absolute z-10 bottom-4 md:bottom-10 right-4 md:right-10 bg-black bg-opacity-50 px-4 py-1 rounded-md text-white text-sm font-semibold",
     };
     const navigation: NavigationOptions = {
         nextEl: ".swiper-next-button",
@@ -29,36 +30,17 @@ const REDImageSlider = () => {
                 className="relative w-full mb-8 bg-gray-100 md:rounded-lg real-estate-detail-slider aspect-video"
             >
                 <SwiperSlide>
-                    <div className="relative w-full h-full">
-                        <div className="absolute inset-0 -z-10 blur-xl bg-white/30">
-                            <img
-                                src="https://res.cloudinary.com/baolocre-estatesone/image/upload/q_auto,f_auto/v1648441637/baolocre_estate/h4ryl3h8nvvtuwc009mj.jpg"
-                                alt="Encacap"
-                                className="object-cover object-center w-full h-full"
-                            />
-                        </div>
-                        <img
-                            src="https://res.cloudinary.com/baolocre-estatesone/image/upload/q_auto,f_auto/v1648441637/baolocre_estate/h4ryl3h8nvvtuwc009mj.jpg"
-                            alt="Encacap"
-                            className="object-contain object-center w-full h-full"
-                        />
-                    </div>
+                    <REDImageSliderImage
+                        src="https://res.cloudinary.com/baolocre-estatesone/image/upload/q_auto,f_auto/v1648441637/baolocre_estate/h4ryl3h8nvvtuwc009mj.jpg"
+                        alt="Real Estate"
+                        priority
+                    />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className="relative w-full h-full">
-                        <div className="absolute inset-0 -z-10 blur-xl bg-white/30">
-                            <img
-                                src="https://res.cloudinary.com/baolocre-estatesone/image/upload/q_auto,f_auto/v1648441637/baolocre_estate/h4ryl3h8nvvtuwc009mj.jpg"
-                                alt="Encacap"
-                                className="object-cover object-center w-full h-full"
-                            />
-                        </div>
-                        <img
-                            src="https://res.cloudinary.com/baolocre-estatesone/image/upload/q_auto,f_auto/v1648441637/baolocre_estate/h4ryl3h8nvvtuwc009mj.jpg"
-                            alt="Encacap"
-                            className="object-contain object-center w-full h-full"
-                        />
-                    </div>
+                    <REDImageSliderImage
+                        src="https://res.cloudinary.com/baolocre-estatesone/image/upload/q_auto,f_auto/v1648441637/baolocre_estate/h4ryl3h8nvvtuwc009mj.jpg"
+                        alt="Real Estate"
+                    />
                 </SwiperSlide>
             </Swiper>
             <REDImageSliderNextButton />
