d97b339fd61bbb8702bbfef8f892b6b8d6446175
Nguyen Khac Khanh <encacap@gmail.com>
2022-06-11 17:02:44 +0700

    build: update ecosystem.config.js

--- ecosytem.config.js
+++ ecosytem.config.js
@@ -4,10 +4,7 @@ module.exports = {
             name: "baolocre.encacap.com",
             script: "yarn",
             args: "start",
-            env_staging: {
-                NODE_ENV: "production",
-                NEXT_PUBLIC_WEBSITE_NAME: "Bất động sản nghỉ dưỡng Bảo Lộc",
-            },
+            env: process.env,
         },
     ],
 };
