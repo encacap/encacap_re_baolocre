3319951a4865a29bedba6ce5afe0c015ca0df690
Nguyen Khac Khanh <encacap@gmail.com>
2022-06-11 16:39:32 +0700

    chore(server): update server config for production

--- server.js
+++ server.js
@@ -1,31 +1,48 @@
 /* eslint-disable security/detect-non-literal-fs-filename */
 const fs = require("fs");
-const { createServer } = require("https");
+const { createServer: createSSLServer } = require("https");
+const { createServer: createNormalServer } = require("http");
 const next = require("next");
 
-const HOSTNAME = "baolocre.dev";
-const PORT = parseInt(process.env.PORT, 10) || 3000;
+const HOSTNAME = process.env.SERVER_HOSTNAME || "localhost";
+const PORT = parseInt(process.env.SERVER_PORT, 10) || 22401;
 const isDevEnvironment = process.env.NODE_ENV !== "production";
 
 const app = next({ dev: isDevEnvironment, hostname: HOSTNAME, port: PORT });
 const handler = app.getRequestHandler();
 const certFolder = "./certs";
 
-const options = {
-    key: fs.readFileSync(`${certFolder}/baolocre.dev.pem`),
-    cert: fs.readFileSync(`${certFolder}/baolocre.dev.crt`),
+const startSSLServer = async () => {
+    const options = {
+        key: fs.readFileSync(`${certFolder}/baolocre.dev.pem`),
+        cert: fs.readFileSync(`${certFolder}/baolocre.dev.crt`),
+    };
+
+    app.prepare().then(() => {
+        createSSLServer(options, handler).listen(
+            {
+                host: HOSTNAME,
+                port: PORT,
+            },
+            () => {
+                // eslint-disable-next-line no-console
+                console.log(`> Ready on https://${HOSTNAME}:${PORT}`);
+            }
+        );
+    });
 };
 
-app.prepare().then(() => {
-    createServer(options, handler).listen(
-        {
-            host: HOSTNAME,
-            port: PORT,
-        },
-        () => {
+const startNormalServer = async () => {
+    app.prepare().then(() => {
+        createNormalServer(handler).listen(PORT, () => {
             // eslint-disable-next-line no-console
-            console.log(`> Ready on https://${HOSTNAME}:${PORT}`);
-            // eslint-disable-next-line comma-dangle
-        }
-    );
-});
+            console.log(`> Ready on http://${HOSTNAME}:${PORT}`);
+        });
+    });
+};
+
+if (isDevEnvironment) {
+    startSSLServer();
+} else {
+    startNormalServer();
+}
